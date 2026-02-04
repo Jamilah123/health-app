b933295c7b6c80662ef749ad1e0661c10751922c
Jamilah123 <moon2004100@gmail.com>
2026-02-04 09:29:22 +0300

    project files upload

---
+++ health app/health app/health_appApp.swift
@@ -0,0 +1,17 @@
+//
+//  health_appApp.swift
+//  health app
+//
+//  Created by Jamilah Jaber Hazazi on 13/08/1447 AH.
+//
+
+import SwiftUI
+
+@main
+struct health_appApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ health app/health appTests/health_appTests.swift
@@ -0,0 +1,17 @@
+//
+//  health_appTests.swift
+//  health appTests
+//
+//  Created by Jamilah Jaber Hazazi on 13/08/1447 AH.
+//
+
+import Testing
+@testable import health_app
+
+struct health_appTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
